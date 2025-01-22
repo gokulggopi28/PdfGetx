7d1d184a7fbd959ba285dc2ca622b740d781818e
Gokul <gokul@realsofterp.com>
2025-01-22 15:14:36 +0530

    add pdf border

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -5,6 +5,7 @@ import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 
 
+
 class HomeController extends GetxController {
  
  final RxList<Map<String, dynamic>> invoices = <Map<String, dynamic>>[].obs;
@@ -31,68 +32,113 @@ class HomeController extends GetxController {
   }
 
   Future<void> downloadInvoicePDF(Map<String, dynamic> invoice) async {
-    try {
-      final pdf = pw.Document();
+  try {
+    final pdf = pw.Document();
 
-      pdf.addPage(
-        pw.Page(
-          build: (pw.Context context) => pw.Column(
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) => pw.Container(
+          decoration: pw.BoxDecoration(
+            border: pw.Border.all( width: 2),
+          ),
+          padding: pw.EdgeInsets.all(16),
+          child: pw.Column(
             crossAxisAlignment: pw.CrossAxisAlignment.center,
             children: [
-              pw.Text('Invoice', style: pw.TextStyle(fontSize: 24, fontWeight: pw.FontWeight.bold)),
+              pw.Text(
+                'Invoice',
+                style: pw.TextStyle(fontSize: 24, fontWeight: pw.FontWeight.bold),
+              ),
               pw.SizedBox(height: 20),
-              pw.Text('Invoice Number: ${invoice['invoiceNumber']}', style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 18)),
-              pw.Text('Customer: ${invoice['customerName']}', style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 18)),
-              pw.Text('Date: ${invoice['date']}', style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 18)),
+              pw.Text(
+                'Invoice Number: ${invoice['invoiceNumber']}',
+                style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 18),
+              ),
+              pw.Text(
+                'Customer: ${invoice['customerName']}',
+                style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 18),
+              ),
+              pw.Text(
+                'Date: ${invoice['date']}',
+                style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 18),
+              ),
               pw.SizedBox(height: 20),
               pw.Table(
                 border: pw.TableBorder.all(),
                 children: [
                   pw.TableRow(
                     children: [
-                      pw.Text('Description', style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 18)),
-                      pw.Text('Quantity', style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 18)),
-                      pw.Text('Price', style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 18)),
-                      pw.Text('Total', style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 18)),
+                      pw.Text(
+                        'Description',
+                        style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 18),
+                      ),
+                      pw.Text(
+                        'Quantity',
+                        style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 18),
+                      ),
+                      pw.Text(
+                        'Price',
+                        style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 18),
+                      ),
+                      pw.Text(
+                        'Total',
+                        style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 18),
+                      ),
                     ],
                   ),
                   ...List.generate(
-                    (invoice['items'] as List).length, 
+                    (invoice['items'] as List).length,
                     (index) {
                       final item = invoice['items'][index];
                       return pw.TableRow(
                         children: [
-                          pw.Text(item['description'], style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 16)),
-                          pw.Text(item['quantity'].toString(), style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 16),textAlign: pw.TextAlign.center),
-                          pw.Text('\$${item['price'].toStringAsFixed(2)}', style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 16)),
-                          pw.Text('\$${(item['quantity'] * item['price']).toStringAsFixed(2)}', style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 16)),
+                          pw.Text(
+                            item['description'],
+                            style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 16),
+                          ),
+                          pw.Text(
+                            item['quantity'].toString(),
+                            style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 16),
+                            textAlign: pw.TextAlign.center,
+                          ),
+                          pw.Text(
+                            '\$${item['price'].toStringAsFixed(2)}',
+                            style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 16),
+                          ),
+                          pw.Text(
+                            '\$${(item['quantity'] * item['price']).toStringAsFixed(2)}',
+                            style: pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 16),
+                          ),
                         ],
                       );
-                    }
+                    },
                   ),
                 ],
               ),
               pw.SizedBox(height: 20),
-              pw.Text('Total Amount: \$${invoice['totalAmount'].toStringAsFixed(2)}', 
-                style: pw.TextStyle(fontSize: 18, fontWeight: pw.FontWeight.bold)),
+              pw.Text(
+                'Total Amount: \$${invoice['totalAmount'].toStringAsFixed(2)}',
+                style: pw.TextStyle(fontSize: 18, fontWeight: pw.FontWeight.bold),
+              ),
             ],
           ),
         ),
-      );
+      ),
+    );
 
-      // Save PDF to device
-      final output = await getTemporaryDirectory();
-      final file = File('${output.path}/invoice_${invoice['invoiceNumber']}.pdf');
-      await file.writeAsBytes(await pdf.save());
+    // Save PDF to device
+    final output = await getTemporaryDirectory();
+    final file = File('${output.path}/invoice_${invoice['invoiceNumber']}.pdf');
+    await file.writeAsBytes(await pdf.save());
 
-      
-      await Printing.sharePdf(bytes: await pdf.save(), filename: 'invoice_${invoice['invoiceNumber']}.pdf');
+    await Printing.sharePdf(bytes: await pdf.save(), filename: 'invoice_${invoice['invoiceNumber']}.pdf');
 
-      Get.snackbar('Success', 'Invoice PDF downloaded successfully!',
-          snackPosition: SnackPosition.BOTTOM);
-    } catch (e) {
-      Get.snackbar('Error', 'Failed to download invoice: $e',
-          snackPosition: SnackPosition.BOTTOM);
-    }
+    Get.snackbar('Success', 'Invoice PDF downloaded successfully!',
+        snackPosition: SnackPosition.BOTTOM);
+  } catch (e) {
+    Get.snackbar('Error', 'Failed to download invoice: $e',
+        snackPosition: SnackPosition.BOTTOM);
   }
+}
+
 }
